635d6c0433cce36aaeb26ae89570e2e641a0d0e2
gourav supakar <gourabsupakar2209@gmail.com>
2024-01-27 18:59:18 +0530

    initial commit wit 2 users

---
+++ call.js
@@ -0,0 +1,24 @@
+
+
+function setUserName(userName){
+    this.userName = userName;
+    console.log(userName);
+    console.log("called");
+};
+
+function createUser(username , email, password){
+
+
+    setUserName.call(this, username);
+    console.log(this.username);
+    this.email = email;
+    this.password = password;
+
+}
+
+const user1 = new createUser("Gourav", "gourav@meta.com", "qwerty");
+
+const user2 = new createUser("Koushik", "supakar@gmail.com", "supakar123");
+
+console.log(user1);
+console.log(user2);
